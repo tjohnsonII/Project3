abf7a83f8982dd6343f66ab575832d2b42d699f3
Timothy A Johnson II <tjohnson082083@gmail.com>
2023-04-23 21:35:35 -0400

    update

--- script.js
+++ script.js
@@ -21,3 +21,85 @@ function validateForm() {
       return;
     }
 }
+
+// Get the number of available seats for each time slot from the server
+fetch('./seats.json')
+  .then(response => response.json())
+  .then(data => {
+    const morningSeats = data.morning;
+    const afternoonSeats = data.afternoon;
+    const eveningSeats = data.evening;
+
+    // Update the table with the number of available seats
+    document.getElementById('morning-seats').textContent = morningSeats;
+    document.getElementById('afternoon-seats').textContent = afternoonSeats;
+    document.getElementById('evening-seats').textContent = eveningSeats;
+
+    // Block fully booked time slots
+    if (morningSeats === 0) {
+      document.querySelector('option[value=morning]').disabled = true;
+    }
+    if (afternoonSeats === 0) {
+      document.querySelector('option[value=afternoon]').disabled = true;
+      }
+      if (eveningSeats === 0) {
+      document.querySelector('option[value=evening]').disabled = true;
+      }
+      })
+      .catch(error => {
+      console.error('Error fetching seats data:', error);
+      });
+      
+      // Handle form submission
+      const form = document.querySelector('form');
+      form.addEventListener('submit', event => {
+      event.preventDefault();
+      
+      // Get form data
+      const formData = new FormData(form);
+      const id = formData.get('id');
+      const firstName = formData.get('first_name');
+      const lastName = formData.get('last_name');
+      const projectTitle = formData.get('project_title');
+      const email = formData.get('email');
+      const phoneNumber = formData.get('phone_number');
+      const timeSlot = formData.get('time_slot');
+      
+      // Check if student has already registered
+      fetch('./students.json')
+      .then(response => response.json())
+      .then(data => {
+      const registeredStudents = data.students;
+      const isRegistered = registeredStudents.some(student => student.id === id);
+      if (isRegistered) {
+        alert('You have already registered for WebTech');
+      } else {
+        // Register student
+        fetch('./register.php', {
+          method: 'POST',
+          body: JSON.stringify({
+            id: id,
+            first_name: firstName,
+            last_name: lastName,
+            project_title: projectTitle,
+            email: email,
+            phone_number: phoneNumber,
+            time_slot: timeSlot
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            alert(data.message);
+          })
+          .catch(error => {
+            console.error('Error registering student:', error);
+          });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching registered students:', error);
+    });
+  });
